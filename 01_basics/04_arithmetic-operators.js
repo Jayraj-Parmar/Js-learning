2396a3eff21d476039e7ebbff5e9dcbd72bcb0a6
Jayraj Parmar <jayrajsinhp340@gmail.com>
2025-02-23 19:05:21 +0530

    rename file data types operations
    just rename file name

---
+++ 01_basics/04_arithmetic-operators.js
@@ -0,0 +1,11 @@
+console.log("10" + 2);
+console.log("10" - 2);
+console.log("10" * 2);
+console.log("10" / 2);
+console.log(2 + "10");
+console.log("10" + 2 + 2);
+console.log(2 + 2 + "10");
+console.log(+"1.1" + +"1.1");
+console.log("1.1" + -"1.1");
+console.log("1.1" - +"1.1");
+console.log(+true);

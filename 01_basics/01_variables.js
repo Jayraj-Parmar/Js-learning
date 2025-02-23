cd4ad3d7c2226830cea93db7a32b18e2c17df840
Jayraj Parmar <jayrajsinhp340@gmail.com>
2025-02-23 18:50:02 +0530

    Create 01_variable
    about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+// What does Dynamic Language mean?
+// JavaScript is a dynamic language and not static, which means that variables can hold
+// values of different types during runtime. Unlike languages such as Typescript or Java,
+// you don't need to declare the data type of a variable explicitly.
+
+const accountId = 1234; //Not Change if overwrite
+//try to overwrite value of const variable
+accountId = 2; //TypeError: Assignment to constant variable
+
+const myObject = { key: "value" };
+myObject.key = "otherValue";
+console.log(myObject); //{ key: 'otherValue' }
+
+const myArray = ["HTML", "CSS"];
+myArray.push("JAVASCRIPT");
+console.log(myArray); // ['HTML', 'CSS', 'JAVASCRIPT'];
+
+// var
+var accountEmail = "jayraj@gmail.com";
+// Not use var variable type in coding because of issue in block scope and functional scope
+
+// let
+let accountCity = "Rajkot";
+accountState = "Gujarat";
+let name;
+
+console.table([accountId, accountEmail, accountCity, accountState, name]);
